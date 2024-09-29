b6308cbfc2cc8f98323bdb920fd0a9aa13f00c00
A01750311 <a01750311@tec.mx>
2024-09-28 21:56:25 -0600

    Mid Section Done

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -8,86 +8,76 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import "./Table.css";
 
-function createData(name, trackingId, date, status) {
-  return { name, trackingId, date, status };
+function createData(id_Producto, Orden, Total, Direccion, Estatus) {
+  return { id_Producto, Orden, Total, Direccion, Estatus };
 }
 
 const rows = [
-  createData("Lasania Chiken Fri", 18908424, "2 March 2022", "Approved"),
-  createData("Big Baza Bang ", 18908424, "2 March 2022", "Pending"),
-  createData("Mouth Freshner", 18908424, "2 March 2022", "Approved"),
-  createData("Cupcake", 18908421, "2 March 2022", "Delivered"),
-  createData("Cupcake", 18908421, "2 March 2022", "Delivered"),
-  createData("Cupcake", 18908421, "2 March 2022", "Delivered"),
-  createData("Cupcake", 18908421, "2 March 2022", "Delivered"),
-  createData("Cupcake", 18908421, "2 March 2022", "Delivered"),
-  createData("Cupcake", 18908421, "2 March 2022", "Delivered"),
-  createData("Cupcake", 18908421, "2 March 2022", "Delivered"),
-  createData("Cupcake", 18908421, "2 March 2022", "Delivered"),
+  createData(10, 159, 222, "Izcalli", "Envíado"),
+  createData(11, 237, 262, "Tlalnepantla", "Pendiente"),
+  createData(12, 262, 305, "Atizapan", "Recibido"),
+  createData(13, 305, 356, "Toluca", "Recibido"),
+  createData(14, 356, 356, "Izcalli", "Envíado"),
+
 ];
 
-
-const makeStyle=(status)=>{
-  if(status === 'Approved')
-  {
+const makeStyle = (status) => {
+  if (status === 'Envíado') {
     return {
       background: 'rgb(145 254 159 / 47%)',
       color: 'green',
-    }
-  }
-  else if(status === 'Pending')
-  {
-    return{
+    };
+  } else if (status === 'Pendiente') {
+    return {
       background: '#ffadad8f',
       color: 'red',
-    }
-  }
-  else{
-    return{
+    };
+  } else {
+    return {
       background: '#59bfff',
       color: 'white',
-    }
+    };
   }
-}
+};
 
 export default function BasicTable() {
   return (
-      <div className="Table">
+    <div className="Table">
       <h3>Recent Orders</h3>
-        <TableContainer
-          component={Paper}
-          style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
-        >
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Product</TableCell>
-                <TableCell align="left">Tracking ID</TableCell>
-                <TableCell align="left">Date</TableCell>
-                <TableCell align="left">Status</TableCell>
-                <TableCell align="left"></TableCell>
+      <TableContainer
+        component={Paper}
+        style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
+      >
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Id_Producto</TableCell>
+              <TableCell align="left">Orden</TableCell>
+              <TableCell align="left">Total$</TableCell>
+              <TableCell align="left">Dirección</TableCell>
+              <TableCell align="left">Estatus</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody style={{ color: "white" }}>
+            {rows.map((row) => (
+              <TableRow
+                key={row.id_Producto} 
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id_Producto} {}
+                </TableCell>
+                <TableCell align="left">{row.Orden}</TableCell>
+                <TableCell align="left">{row.Total}</TableCell>
+                <TableCell align="left">{row.Direccion}</TableCell>
+                <TableCell align="left">
+                  <span className="status" style={makeStyle(row.Estatus)}>{row.Estatus}</span>
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody style={{ color: "white" }}>
-              {rows.map((row) => (
-                <TableRow
-                  key={row.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="left">{row.trackingId}</TableCell>
-                  <TableCell align="left">{row.date}</TableCell>
-                  <TableCell align="left">
-                    <span className="status" style={makeStyle(row.status)}>{row.status}</span>
-                  </TableCell>
-                  <TableCell align="left" className="Details">Details</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
