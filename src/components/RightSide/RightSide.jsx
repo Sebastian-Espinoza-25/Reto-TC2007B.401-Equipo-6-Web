94d4438fb09ca6ee8fd2f762ffa9dd5b3684aedb
A01750311 <a01750311@tec.mx>
2024-09-28 23:52:24 -0600

    Dashboard 100% responsivo

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import Updates from '../Updates/Updates';
 import './RightSide.css';
+import RightGraph from '../RightGraph/RightGraph';
+
 
 const RightSide = () => {
     return (
-        <div>RightSide</div>
+        <div className="RightSide">
+            <div>
+
+                <h3>Updates</h3>
+                <Updates/>
+            </div>
+            <div>
+                <h3>Graph</h3>
+                <RightGraph/>
+            </div>
+            
+            
+            
+        </div>
+        
     )
 }
 
