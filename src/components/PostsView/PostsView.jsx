7622dd666001145f78ca4cfe2c43ed0c690d8445
Julio Vivas <jcvmlego@gmail.com>
2024-10-04 23:14:54 -0600

    PostView Actualizada

--- src/components/PostsView/PostsView.jsx
+++ src/components/PostsView/PostsView.jsx
@@ -27,24 +27,74 @@ const PostsView = () => {
     post.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const handleDelete = async (post_id) => {
+    const confirmDelete = window.confirm(
+      "¿Estás seguro de que deseas eliminar este post?"
+    );
+    if (confirmDelete) {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/post//posts/${post_id}`,
+          {
+            method: "DELETE",
+          }
+        );
+        if (response.ok) {
+          setPosts(posts.filter((post) => post.post_id !== post_id));
+        }
+      } catch (error) {
+        console.error("Error al eliminar el post:", error);
+      }
+    }
+  };
+
+  const togglePostStatus = async (post_id) => {
+    const updatedPosts = posts.map((post) => {
+      if (post.post_id === post_id) {
+        return { ...post, enabled: !post.enabled };
+      }
+      return post;
+    });
+    setPosts(updatedPosts);
+
+    // TODO: Lógica para actualizar el estado del producto en la base de datos
+  };
+
   return (
     <div className="posts-view">
-      <h1>Lista de Posts</h1>
-      <input
-        type="text"
-        placeholder="Buscar por título"
-        value={searchTerm}
-        onChange={handleSearch}
-        className="search-bar"
-      />
+      <div className="header">
+        <h2>Lista de Productos</h2>
+        <input
+          type="text"
+          placeholder="Buscar por titulo"
+          value={searchTerm}
+          onChange={handleSearch}
+          className="search-bar"
+        />
+      </div>
       <div className="posts-container">
         {filteredPosts.map((post) => (
           <div className="post-card" key={post.post_id}>
-            <img src={`http://localhost${post.image_path}`} alt={post.title} />
+            <img src={`http://localhost/${post.file_path}`} alt={post.title} />
             <div className="post-info">
               <h2>{post.title}</h2>
-              <p>{post.summary}</p>
-              <p>Categoría: {post.date}</p>
+              <p className="post-summary">{post.summary}</p>
+              <p>Categoría: {post.category}</p>
+              <p>Fecha: {post.date}</p>
+              <button
+                className="delete-button"
+                onClick={() => handleDelete(post.post_id)}
+              >
+                ❌
+              </button>
+              <button
+                className={`toggle-button ${
+                  post.enabled ? "enabled" : "disabled"
+                }`}
+                onClick={() => togglePostStatus(post.post_id)}
+              >
+                {posts.enabled ? "Deshabilitar" : "Habilitar"}
+              </button>
             </div>
           </div>
         ))}
